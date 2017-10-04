93740b35637f32766ca0b71f3bd10bdb63bda701
Roy Hsu <roy.hsu@tinyworld.cc>
2017-10-04 22:27:11 +0800

    tweaks

--- APIClient/PostViewController.swift
+++ APIClient/PostViewController.swift
@@ -1,19 +1,25 @@
 //
-//  ViewController.swift
+//  PostViewController.swift
 //  APIClient
 //
 //  Created by Roy Hsu on 04/10/2017.
 //  Copyright © 2017 TinyWorld. All rights reserved.
 //
 
+// MARK: - PostViewController
+
 import UIKit
 
-public final class ViewController: UIViewController {
+public final class PostViewController: UIViewController {
 
+    // MARK: Property
+    
     public final let client: PostAPIClient = APIClient(
         router: APIRouter.readPost(id: "1"),
         httpClient: SimpleHTTPClient()
     )
+    
+    // MARK: View Life Cycle
     
     public final override func viewDidLoad() {
         super.viewDidLoad()
